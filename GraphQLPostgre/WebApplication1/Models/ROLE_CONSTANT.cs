7a60b023f40a63deb6ca852f360711cde99a65e7
jaythien288 <49481338+jaythien288@users.noreply.github.com>
2021-03-16 23:57:10 +0700

    confix authorization role

---
+++ GraphQLPostgre/WebApplication1/Models/ROLE_CONSTANT.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public static class ROLE_CONSTANT
+    {
+        public const string ADMIN = "Admin";
+        public const string FARMER = "Farmer";
+    }
+}
